3e46fd75b3668028b1fa55b1543dade96ed81ee0
Yehonatan Eldar <yehonatan.eldar2@gmail.com>
2025-03-30 18:05:48 +0300

    host functionality, better pantry class

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
+import os
 import random
 
-from flask import Flask, render_template
+import qrcode
+from flask import Flask, redirect, render_template, request, url_for
+from PIL import Image
 
 from pantry import Pantry
-
 
 app = Flask(__name__)
 
 pantry = Pantry()
+print('Deleting existing baskets...')
+pantry.delete_all_baskets()
+print('Baskets deleted successfully!')
 
 def create_party():
-    party_id = random.randint(100, 1000)
+    party_id = random.randint(1000, 10000)
     
-    pantry.put(
+    pantry.new_basket(
         basket_name=f'party_{party_id}', 
         data={
             'id': party_id,
@@ -21,20 +26,71 @@ def create_party():
         }
     )
 
+    return party_id
+
+def generate_qr_code(data, filename="qrcode.png", box_size=10, border=4, fill_color="black", back_color="white"):
+    """
+    Generate a QR code from the given data
+    
+    Parameters:
+    data (str): The data to encode in the QR code
+    filename (str): Output filename for the QR code image
+    box_size (int): Size of each box in the QR code
+    border (int): Border size around the QR code
+    fill_color (str): Color of the QR code data
+    back_color (str): Background color
+    
+    Returns:
+    str: Path to the saved QR code image
+    """
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_H,
+        box_size=box_size,
+        border=border,
+    )
+    
+    qr.add_data(data)
+    qr.make(fit=True)
+    
+    img = qr.make_image(fill_color=fill_color, back_color=back_color)
+    
+    directory = os.path.dirname(filename)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    img.save(filename)
+    
+    return os.path.abspath(filename)
+
 @app.route('/')
 def home():
-    # Button to start a party
     return render_template('index.html')
 
 @app.route('/party/<id>')
 def party(id):
     data = pantry.get(f'party_{id}')
-    # QR code for joining
-    # Host feature
-    # Host can add or delete streaming services
-    # Host can remove participants
-    # Host can start party
-    return render_template('party.html')
+    
+    if not data:
+        return f"No data found for party ID {id}", 404
+    
+    isHost = request.args.get("isHost")
+    
+    url_root = request.url_root
+    join_url = f'{url_root}join_party/{id}'
+    image_filename = f'qrcode_{id}.png'
+    generate_qr_code(join_url, filename=os.path.join('qr_codes', image_filename))
+    
+    return render_template('party.html', image_filename=image_filename, join_url=join_url, id=id, isHost=isHost, data=data)
+
+@app.route('/create_party')
+def create_party_page():
+    id = create_party()
+    return redirect(url_for('party', id=id) + '?isHost=True')
+
+@app.route('/join_party/<id>')
+def join_party_page(id):
+    return redirect(url_for('party', id=id) + '?isHost=False')
 
 if __name__ == '__main__':
     app.run()
--- pantry.py
+++ pantry.py
@@ -1,7 +1,9 @@
+import json
+import os
+import time
+
 import requests
 from dotenv import load_dotenv
-import os
-import json
 
 load_dotenv()
 
@@ -9,26 +11,58 @@ class Pantry:
     
     def __init__(self):
         self.id = os.getenv('PANTRY_ID')
-        url = f"https://getpantry.cloud/apiv1/pantry/{self.id}/basket/"
+        self.url = f"https://getpantry.cloud/apiv1/pantry/{self.id}"
+
+    def base_func(self, action: str, data: dict | None, url: str):
+        time.sleep(0.6)
+        if data:
+            payload = json.dumps(data)
+        else:
+            payload = ''
+
+
+        headers = {
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request(action, url, headers=headers, data=payload)
+
+        try:
+
+            return response.json()
+        
+        except requests.exceptions.JSONDecodeError:
+            return response.text
+
+
+    def get_data(self):
+
+        return self.base_func("GET", None, self.url)
 
     def get(self, basket_name: str):
+        new_url = f'{self.url}/basket/{basket_name}'
 
-        payload = ""
-        headers = {
-        'Content-Type': 'application/json'
-        }
-
-        response = requests.request("GET", self.url + basket_name, headers=headers, data=payload)
-
-        return response.json()
+        return self.base_func("GET", None, new_url)
 
     def put(self, basket_name: str, data: dict):
-        payload = json.dumps(data)
+        new_url = f'{self.url}/basket/{basket_name}'
+        
+        return self.base_func("PUT", data, new_url)
     
-        headers = {
-        'Content-Type': 'application/json'
-        }
+    def new_basket(self, basket_name: str, data: dict):
+        new_url = f'{self.url}/basket/{basket_name}'
+        
+        return self.base_func("POST", data, new_url)
+    
+    def delete_basket(self, basket_name):
+        new_url = f'{self.url}/basket/{basket_name}'
+        return self.base_func("DELETE", None, new_url)
+    
+    def delete_all_baskets(self):
+        data = self.get_data()
+        basket_list = [d['name'] for d in data['baskets']]
 
-        response = requests.request("PUT", self.url + basket_name, headers=headers, data=payload)
+        for name in basket_list:
+            self.delete_basket(name)
 
-        return response.json()
+
